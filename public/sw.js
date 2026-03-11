18c472037504cbe8731afe5ee18c81dab0b8b2b6
Mathilde Cornelis <mathilde.cornelis26@gmail.com>
2026-03-11 12:58:11 +0100

    icônes

--- public/sw.js
+++ public/sw.js
@@ -2,8 +2,8 @@ self.addEventListener('push', (event) => {
   const data = event.data ? event.data.json() : {};
   self.registration.showNotification(data.title || '⚠️ Mon Frigo', {
     body: data.body || '',
-    icon: '/favicon.ico',
-    badge: '/favicon.ico',
+    icon: '/icon-192.png',
+    badge: '/icon-192.png',
   });
 });
 
